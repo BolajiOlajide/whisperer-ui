4cb5ae3da29ac4fe8f4365f24505db139f9c6771
Bolaji Olajide <cooproton@gmail.com>
2020-02-04 01:28:05 +0100

    chore: change prop name :)

--- pages/_app.js
+++ pages/_app.js
@@ -6,23 +6,25 @@ import theme from '../theme';
 
 class WhispererApp extends App {
   static getInitialProps = async ({ Component, ctx }) => {
-    let individualPageProps = {};
+    let pageProps = {};
     if (Component.getInitialProps) {
-      individualPageProps = await Component.getInitialProps(ctx);
+      pageProps = await Component.getInitialProps(ctx);
     }
-    // exposes the query to the user
-    individualPageProps.query = ctx.query;
-    return { individualPageProps };
+    pageProps.query = {
+      ...ctx.query,
+      ...pageProps.query
+    };
+    return { pageProps };
   };
 
   render() {
-    const { Component, individualPageProps } = this.props;
+    const { Component, pageProps } = this.props;
 
     return (
       <ThemeProvider theme={theme}>
         <CSSReset />
         <Container>
-          <Component {...individualPageProps} />
+          <Component {...pageProps} />
         </Container>
       </ThemeProvider>
     )
