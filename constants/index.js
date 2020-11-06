b0aa4515fe2a5f32c065dade1240a7c6d8c663d1
Bolaji Olajide <cooproton@gmail.com>
2020-11-06 10:38:24 +0100

    aaarrggh! update said endpoint again

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 // export const endpoint = 'https://whisperer-api.herokuapp.com/graphql';
-export const endpoint = 'https://6e6271c416fa.ngrok.io/graphql'
+export const endpoint = 'https://717b37aab475.ngrok.io/graphql'
 // export const endpoint = 'http://localhost:5000/graphql';
 
 export const WHISPER_TOKEN = 'X-TOKEN';
